f3591d059ba45e2b1fed18aa83063ec9051e0fe7
Xenock <miguelmoracho@gmail.com>
2017-05-11 18:49:57 +0200

    indenting and killing semicolons

--- awesome-project/controllers/matchController.js
+++ awesome-project/controllers/matchController.js
@@ -1,108 +1,88 @@
-var Match = require('../models/Match.js');
+var Match = require('../models/Match.js')
 
-/**
- * matchController.js
- *
- * @description :: Server-side logic for managing matches.
- */
 module.exports = {
 
-    /**
-     * matchController.list()
-     */
-    list: function (req, res) {
-        Match.find({}, function (err, matches) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting match.',
-                    error: err
-                });
-            }
-            return res.json(matches);
-        });
-    },
+  list: function (req, res) {
+    Match.find({}, function (err, matches) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting match.',
+          error: err
+        })
+      }
+      return res.json(matches)
+    })
+  },
 
-    /**
-     * matchController.show()
-     */
-    show: function (req, res) {
-        var id = req.params.id;
-        Match.findOne({_id: id}, function (err, match) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting match.',
-                    error: err
-                });
-            }
-            if (!team) {
-                return res.status(404).json({
-                    message: 'No such match'
-                });
-            }
-            return res.json(match);
-        });
-    },
+  show: function (req, res) {
+    var id = req.params.id
+    Match.findOne({_id: id}, function (err, match) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting match.',
+          error: err
+        })
+      }
+      if (!team) {
+        return res.status(404).json({
+          message: 'No such match'
+        })
+      }
+      return res.json(match)
+    })
+  },
 
-    /**
-     * matchController.create()
-     */
-    create: function (req, res) {
-      var Match = new Match({      });
+  create: function (req, res) {
+    var Match = new Match({      })
 
-      match.save(function (err, match) {
-          if (err) {
-              return res.status(500).json({
-                  message: 'Error when creating match',
-                  error: err
-              });
-          }
-          return res.status(201).json(match);
-      });
-    },
+    match.save(function (err, match) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when creating match',
+          error: err
+        })
+      }
+      return res.status(201).json(match)
+    })
+  },
 
-    /**
-     * matchController.update()
-     */
-    update: function (req, res) {
-        var id = req.params.id;
-        Match.findOne({_id: id}, function (err, match) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting match',
-                    error: err
-                });
-            }
-            if (!match) {
-                return res.status(404).json({
-                    message: 'No such match'
-                });
-            }
-            Match.save(function (err, match) {
-                if (err) {
-                    return res.status(500).json({
-                        message: 'Error when updating match.',
-                        error: err
-                    });
-                }
+  update: function (req, res) {
+    var id = req.params.id
+    Match.findOne({_id: id}, function (err, match) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting match',
+          error: err
+        })
+      }
+      if (!match) {
+        return res.status(404).json({
+          message: 'No such match'
+        })
+      }
+      Match.save(function (err, match) {
+        if (err) {
+          return res.status(500).json({
+            message: 'Error when updating match.',
+            error: err
+          })
+        }
 
-                return res.json(match);
-            });
-        });
-    },
+        return res.json(match)
+      })
+    })
+  },
 
-    /**
-     * matchController.remove()
-     */
-    remove: function (req, res) {
-        var id = req.params.id;
-        Match.findByIdAndRemove(id, function (err, match) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when deleting the match.',
-                    error: err
-                });
-            }
-            return res.status(204).json();
-        });
-    }
-};
+  remove: function (req, res) {
+    var id = req.params.id
+    Match.findByIdAndRemove(id, function (err, match) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when deleting the match.',
+          error: err
+        })
+      }
+      return res.status(204).json()
+    })
+  }
+}
--- awesome-project/controllers/teamController.js
+++ awesome-project/controllers/teamController.js
@@ -1,108 +1,88 @@
-var Team = require('../models/Team.js');
+var Team = require('../models/Team.js')
 
-/**
- * teamController.js
- *
- * @description :: Server-side logic for managing teams.
- */
 module.exports = {
 
-    /**
-     * teamController.list()
-     */
-    list: function (req, res) {
-        Team.find(function (err, teams) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting team.',
-                    error: err
-                });
-            }
-            return res.json(teams);
-        });
-    },
+  list: function (req, res) {
+    Team.find(function (err, teams) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting team.',
+          error: err
+        })
+      }
+      return res.json(teams)
+    })
+  },
 
-    /**
-     * teamController.show()
-     */
-    show: function (req, res) {
-        var id = req.params.id;
-        Team.findOne({_id: id}, function (err, team) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting team.',
-                    error: err
-                });
-            }
-            if (!team) {
-                return res.status(404).json({
-                    message: 'No such team'
-                });
-            }
-            return res.json(team);
-        });
-    },
+  show: function (req, res) {
+    var id = req.params.id
+    Team.findOne({_id: id}, function (err, team) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting team.',
+          error: err
+        })
+      }
+      if (!team) {
+        return res.status(404).json({
+          message: 'No such team'
+        })
+      }
+      return res.json(team)
+    })
+  },
 
-    /**
-     * teamController.create()
-     */
-    create: function (req, res) {
-        var team = new team({        });
+  create: function (req, res) {
+    var team = new team({        })
 
-        Team.save(function (err, team) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when creating team',
-                    error: err
-                });
-            }
-            return res.status(201).json(team);
-        });
-    },
+    Team.save(function (err, team) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when creating team',
+          error: err
+        })
+      }
+      return res.status(201).json(team)
+    })
+  },
 
-    /**
-     * teamController.update()
-     */
-    update: function (req, res) {
-        var id = req.params.id;
-        Team.findOne({_id: id}, function (err, team) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting team',
-                    error: err
-                });
-            }
-            if (!team) {
-                return res.status(404).json({
-                    message: 'No such team'
-                });
-            }
-            Team.save(function (err, team) {
-                if (err) {
-                    return res.status(500).json({
-                        message: 'Error when updating team.',
-                        error: err
-                    });
-                }
+  update: function (req, res) {
+    var id = req.params.id
+    Team.findOne({_id: id}, function (err, team) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting team',
+          error: err
+        })
+      }
+      if (!team) {
+        return res.status(404).json({
+          message: 'No such team'
+        })
+      }
+      Team.save(function (err, team) {
+        if (err) {
+          return res.status(500).json({
+            message: 'Error when updating team.',
+            error: err
+          })
+        }
 
-                return res.json(team);
-            });
-        });
-    },
+        return res.json(team)
+      })
+    })
+  },
 
-    /**
-     * teamController.remove()
-     */
-    remove: function (req, res) {
-        var id = req.params.id;
-        team.findByIdAndRemove(id, function (err, team) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when deleting the team.',
-                    error: err
-                });
-            }
-            return res.status(204).json();
-        });
-    }
-};
+  remove: function (req, res) {
+    var id = req.params.id
+    team.findByIdAndRemove(id, function (err, team) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when deleting the team.',
+          error: err
+        })
+      }
+      return res.status(204).json()
+    })
+  }
+}
