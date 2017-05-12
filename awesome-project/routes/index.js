1b81c123695f29a5e88553ac29a9bed684103b66
Xenock <miguelmoracho@gmail.com>
2017-05-12 11:40:37 +0200

    showing not finished at index

--- awesome-project/routes/index.js
+++ awesome-project/routes/index.js
@@ -3,7 +3,7 @@ const router  = express.Router();
 const Match = require('../models/Match.js')
 
 router.get('/', (req, res, next) => {
-  Match.find({}, (err, matches) => {
+  Match.find({status: {$ne: 'FINISHED'}}, (err, matches) => {
     if (err) { return next(err) }
     return res.render('index', {matches: matches});
   }).sort({date: 1}).limit(10)
